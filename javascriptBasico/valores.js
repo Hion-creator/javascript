764feb2e2eec89e489fbdf19854bc2f08d7b1170
Andres Salazar <andresfelipe.salazar@rocketfy.cof>
2023-01-10 09:59:44 -0500

    first commit

---
+++ javascriptBasico/valores.js
@@ -0,0 +1,18 @@
+// Descripción: valores primitivos en javascript
+//numericos
+typeof 40
+console.log(typeof 40)
+//string
+"string cadena de texto"
+
+//buleanos
+true
+false
+
+//valores vacios
+null
+undefined
+
+//valores de objetos
+[1,2,3]
+{nombre: "juan"}
---
+++ javascriptBasico/variable.js
@@ -0,0 +1,17 @@
+var nombre = "juan";
+
+// declarar una variable
+var edad;
+// inicializar una variable
+edad = 30;
+
+edad
+
+var elementos = ["computadora", "celular"];
+
+var persona = {
+    nombre: "juan",
+    edad: 30
+}
+
+persona;
